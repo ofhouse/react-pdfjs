29471b5b52914fb60ae69cd738198e0625bc7532
Felix Haus <felixhaus@gmail.com>
2018-09-19 20:53:51 +0200

    Adds core files and build setup

---
+++ rollup.config.js
@@ -0,0 +1,26 @@
+import postcss from 'rollup-plugin-postcss';
+import babel from 'rollup-plugin-babel';
+
+export default {
+  input: 'src/index.js',
+  output: {
+    name: 'react-pdfjs',
+    exports: 'named',
+    globals: {
+      react: 'React',
+    },
+
+    file: 'build/index.js',
+    format: 'cjs',
+  },
+
+  plugins: [
+    babel({
+      exclude: 'node_modules/**',
+    }),
+
+    postcss({
+      extract: true,
+    }),
+  ],
+};
---
+++ src/toolbar.js
@@ -0,0 +1,42 @@
+// @flow
+
+import * as React from 'react';
+
+import { withPdfJsContext } from './react-pdfjs';
+import type { ToolbarContext } from './types';
+
+type RenderProps = {
+  zoomIn: () => any,
+};
+
+type Props = {
+  render?: (props: RenderProps) => any,
+  children?: (props: RenderProps) => any,
+  reactPdfjs: ToolbarContext,
+};
+
+class Toolbar extends React.Component<Props> {
+  render() {
+    const { reactPdfjs } = this.props;
+
+    const renderProps: RenderProps = {
+      zoomIn: reactPdfjs.zoomIn,
+      zoomOut: reactPdfjs.zoomOut,
+      scrollToPage: reactPdfjs.scrollToPage,
+    };
+
+    if (this.props.render) {
+      return this.props.render(renderProps);
+    }
+
+    if (this.props.children) {
+      return this.props.children(renderProps);
+    }
+
+    return null;
+  }
+}
+
+const ConnectedToolbar = withPdfJsContext(Toolbar);
+
+export { ConnectedToolbar as Toolbar };
