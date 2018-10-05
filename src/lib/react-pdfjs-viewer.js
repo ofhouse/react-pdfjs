b531ae66efb5cd666e0889cab2ba33a15dbd2ed8
Felix Haus <felixhaus@gmail.com>
2018-10-05 16:33:26 +0200

    Implement custom scrollPageIntoView

--- src/lib/react-pdfjs-viewer.js
+++ src/lib/react-pdfjs-viewer.js
@@ -34,6 +34,44 @@ class ReactPDFJSViewer extends PDFViewer {
       renderAnnotation: this.renderAnnotation,
     });
   }
+
+  /**
+   *
+   */
+  scrollPageIntoView(params) {
+    if (!this.pdfDocument) {
+      return;
+    }
+    let pageNumber = params.pageNumber || 0;
+    let dest = params.destArray || null;
+
+    if (this.isInPresentationMode || !dest) {
+      this._setCurrentPageNumber(pageNumber, /* resetCurrentPageView = */ true);
+      return;
+    }
+
+    const pageView = this._pages[pageNumber - 1];
+    const { scale, div } = pageView;
+
+    const x = dest[2];
+    const y = dest[3];
+    const width = dest[4];
+    const height = dest[5];
+
+    const scrollTop =
+      div.offsetTop + pageView.viewport.height * y - pageView.viewport.height * 0.05;
+
+    // Scroll to the right edge of the box and subtract the width of the container so that we have
+    // more negative space to the left
+    const scrollLeft =
+      div.offsetLeft +
+      pageView.viewport.width * (x + width) -
+      this.container.clientWidth +
+      pageView.viewport.width * 0.05;
+
+    this.container.scrollTop = scrollTop;
+    this.container.scrollLeft = scrollLeft;
+  }
 }
 
 export default ReactPDFJSViewer;
