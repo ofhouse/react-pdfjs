71ec7918dca328cabfbd5de2f0d2a3446ac7daf4
Felix Haus <felixhaus@gmail.com>
2022-05-19 11:41:57 +0200

    Delete old content

--- example/src/react-pdfjs/index.js
+++
@@ -1,7 +0,0 @@
-// @flow
-
-import './styles/text_layer_builder.css';
-
-export { Toolbar } from './toolbar';
-export { Viewer } from './viewer';
-export { ReactPdfjs as default } from './react-pdfjs';
